7da15a9d9c9afd22c71f304b39274b3abebddf56
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-02 20:56:55 +0300

    navigate with Link component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const PageOne = () =>{
-    return <div>PageOne</div>;
+    return (
+        <div>
+            PageOne<br/>
+            <Link to="/pagetwo">Navigate to Page Two</Link>
+        </div>
+    );
 };
 
 const PageTwo = () =>{
-    return <div>
-        PageTwo
-        <button>Click Me</button>
-        </div>;
+    return (
+        <div>
+            PageTwo<br/>
+            <button>Click Me</button><br/>
+            <Link to="/">Navigate to Page One</Link>
+        </div>
+    );
 }
 
 const App = () =>{
