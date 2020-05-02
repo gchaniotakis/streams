8261539646ff94179c82226025f03867a1d4697e
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-03 00:23:49 +0300

    components routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-const PageOne = () =>{
-    return (
-        <div>
-            PageOne<br/>
-            <Link to="/pagetwo">Navigate to Page Two</Link>
-        </div>
-    );
-};
 
-const PageTwo = () =>{
-    return (
-        <div>
-            PageTwo<br/>
-            <button>Click Me</button><br/>
-            <Link to="/">Navigate to Page One</Link>
-        </div>
-    );
-}
 
 const App = () =>{
     return(
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne}/>
-                    <Route path="/pagetwo" component={PageTwo}/>
+                    <Route path='/' exact component={StreamList}/>
+                    <Route path='/streams/new' exact component={StreamCreate}/>
+                    <Route path='/streams/edit' exact component={StreamEdit}/>
+                    <Route path='/streams/delete' exact component={StreamDelete}/>
+                    <Route path='/streams/show' exact component={StreamShow}/>
                 </div>
             </BrowserRouter>
         </div>
