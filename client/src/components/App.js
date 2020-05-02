c185e21696cb8a0f21999800060299b80723cb31
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-02 03:47:55 +0300

    initialized router with dummy pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const PageOne = () =>{
+    return <div>PageOne</div>;
+};
+
+const PageTwo = () =>{
+    return <div>
+        PageTwo
+        <button>Click Me</button>
+        </div>;
+}
 
 const App = () =>{
-    return <div>App</div>;
+    return(
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne}/>
+                    <Route path="/pagetwo" component={PageTwo}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
