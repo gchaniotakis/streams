91c5d8fe32504c219514eac7cc530d714457dcb2
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-03 02:00:44 +0300

    move header inside of browser router/cleanup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -11,10 +11,10 @@ import Header from './Header';
 
 const App = () =>{
     return(
-        <div className="ui container">
-            <Header/>
+        <div className="ui container">            
             <BrowserRouter>
                 <div>
+                    <Header/>
                     <Route path='/' exact component={StreamList}/>
                     <Route path='/streams/new' exact component={StreamCreate}/>
                     <Route path='/streams/edit' exact component={StreamEdit}/>
