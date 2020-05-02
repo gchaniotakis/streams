6d69a6738d8e3ccad31d7782f38a10406a65fa6b
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-03 01:55:53 +0300

    header component initialize

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,12 +5,14 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
 
 const App = () =>{
     return(
-        <div>
+        <div className="ui container">
+            <Header/>
             <BrowserRouter>
                 <div>
                     <Route path='/' exact component={StreamList}/>
