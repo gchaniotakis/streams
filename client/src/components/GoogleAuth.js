d4be55710e290273d3d1807e79c3d5a4f65db4c2
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-03 18:04:57 +0300

    fix error in googleauth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -4,7 +4,7 @@ class GoogleAuth extends React.Component{
     componentDidMount(){
         window.gapi.load('client: auth2', () =>{
             window.gapi.client.init({
-                clientId: '68354859266-3vabbrmi6asop5qhpg8ohbc1vpei8sjp.apps.googleusercontent.com ',
+                clientId: '68354859266-3vabbrmi6asop5qhpg8ohbc1vpei8sjp.apps.googleusercontent.com',
                 scope: 'email'
             });
         });
