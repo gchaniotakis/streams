c6d22f185c7478f7b73b3757e1ce16f7a38d4179
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-09 14:49:38 +0300

    integrate with redux dev tools extension for chrome

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware,compose} from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEhancers(applyMiddleware()));
 
 ReactDOM.render(
     <Provider store={store}>
