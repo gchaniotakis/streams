f3190ca13db0d15282632fd3925b45342413f038
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-11 00:26:57 +0300

    full form rendering/handling submision with redux form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component{
-    renderInput({input}){        
-        return<input {...input}/>;
+    renderInput({input, label}){        
+        return(
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+        );
+    }
+
+    onSubmit(formValues){
+        console.log(formValues);
     }
     
     render(){        
         return(
-            <form>
-                <Field name="title" component={this.renderInput}/>
-                <Field name="description" component={this.renderInput}/>
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" component={this.renderInput} label="Enter Description"/>
+                <button className="ui button primary">Submit</button>
             </form>
         ) 
     }    
