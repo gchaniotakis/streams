88e24645050f6ccdb3bd27eb45de8b3f670633d9
Georgios Chaniotakis <haniotinho@gmail.com>
2020-05-17 23:13:21 +0300

    create stream action creator initlialize

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
+import streams from '../apis/streams';
 import {SIGN_IN, SIGN_OUT} from './types';
+import { formValues } from 'redux-form';
 
 export const signIn = (userId) =>{
     return {
@@ -11,4 +13,8 @@ export const signOut = () =>{
     return {
         type: SIGN_OUT
     };
+};
+
+export const createStream = (formValues) => async (dispatch) =>{
+    streams.post('/streams', formValues);
 };
