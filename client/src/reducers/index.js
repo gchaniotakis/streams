d5d4adc50118d3a98316c0c1d2f8f95885bf234e
Georgios Chaniotakis <haniotinho@gmail.com>
2020-08-23 01:08:02 +0300

    install lodash library/create streams reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import authReducer from './authReducer';
+import streamsReducer from './streamsReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamsReducer
 });
---
+++ client/src/reducers/streamsReducer.js
@@ -0,0 +1,17 @@
+import {FETCH_STREAM,FETCH_STREAMS,CREATE_STREAM,EDIT_STREAM,DELETE_STREAM} from '../actions/types';
+import _ from 'lodash';
+
+export default(state = {}, action) =>{
+    switch(action.type){
+        case FETCH_STREAM:            
+        case CREATE_STREAM:             
+        case EDIT_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
+        case FETCH_STREAMS:
+            return {... state, ..._.mapKeys(action.payload, 'id')};
+        default:
+            return state;
+    }
+};
