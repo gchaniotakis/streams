2d600e34d24bc03be8ccf967fb884e9c764b4f20
Georgios Chaniotakis <haniotinho@gmail.com>
2020-08-23 18:33:40 +0300

    using history to navigate to home page after creating new stream

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM} from './types';
 
 
@@ -19,6 +20,7 @@ export const createStream = (formValues) => async (dispatch, getState) =>{
    const {userId} = getState().auth; 
    const response = await streams.post('/streams', {...formValues,userId});
    dispatch({type: CREATE_STREAM, payload: response.data});
+   history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) =>{
