11596a391608393a12f13a817dc7169ff3a6f3d9
Georgios Chaniotakis <haniotinho@gmail.com>
2020-08-23 23:21:19 +0300

    make StreamForm Component reusable/Refactor StreamCreate/add StreamEdit functionality/modify editStream action as patch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -36,9 +36,10 @@ export const fetchStream = (id) => async (dispatch) =>{
 };
 
 export const editStream = (id, formValues) => async (dispatch) =>{
-    const response = await streams.put(`/streams/${id}`, formValues); 
+    const response = await streams.patch(`/streams/${id}`, formValues); //use patch so some properties are being updated
 
     dispatch({type: EDIT_STREAM, payload: response.data});
+    history.push('/');
 };
 
 export const deleteStream = (id) => async (dispatch) =>{
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchStream} from '../../actions'
+import {fetchStream, editStream} from '../../actions';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
 class StreamEdit extends React.Component{
     componentDidMount(){
         this.props.fetchStream(this.props.match.params.id);
     }
+
+    onSubmit = (formValues) =>{
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
     
     render(){
         
         if(!this.props.stream){
             return <div>Loading...</div>;
         }
-        return <div>{this.props.stream.title}</div>;
+        return (
+        <div>
+            <h3>Edit a Stream</h3>
+            <StreamForm onSubmit={this.onSubmit} initialValues={_.pick(this.props.stream, 'title', 'description')}/>
+        </div>
+        );
     }
 }
 
@@ -22,4 +33,4 @@ const mapStateToProps = (state, ownProps) => {
     
 }
 
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
