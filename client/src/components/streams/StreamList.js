cf0fc1116885d619588aac5481c98764b6d509c9
Georgios Chaniotakis <haniotinho@gmail.com>
2020-09-20 23:56:45 +0300

    manage route through Switch/configure stream show component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -30,7 +30,7 @@ class StreamList extends React.Component{
                     {this.renderAdminButtons(stream)}
                     <i className="large middle aligned icon camera"/>
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header">{stream.title}</Link>
                         <div className="description">{stream.description}</div>
                     </div>                    
                 </div>
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamShow = () =>{
-    return <div>StreamShow</div>;
-};
+class StreamShow extends React.Component{
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-export default StreamShow;
+    render(){
+        if(!this.props.stream){
+            return <div>Loading...</div>;
+        }
+        
+        return(
+            <div>
+                <h1>{this.props.stream.title}</h1>
+                <h5>{this.props.stream.description}</h5>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) =>{
+    return {stream: state.streams[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamShow);
