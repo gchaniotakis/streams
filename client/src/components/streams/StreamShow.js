76831e4a7444eceebd9020ac6c35a403d890e5d3
Georgios Chaniotakis <haniotinho@gmail.com>
2020-09-27 13:04:09 +0300

    stream url typo fix/clean up player after streaming is finished

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -20,6 +20,10 @@ class StreamShow extends React.Component{
         this.buildPlayer();
     }
 
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+
     buildPlayer(){
         if(this.player || !this.props.stream){
             return;
@@ -28,7 +32,7 @@ class StreamShow extends React.Component{
 
         this.player = flv.createPlayer({
             type:'flv',
-            url:`http://localhost:8000/live${id}.flv`
+            url:`http://localhost:8000/live/${id}.flv`
         });
 
         this.player.attachMediaElement(this.videoRef.current);
